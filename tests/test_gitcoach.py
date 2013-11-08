231331aa045ed926b0d92d4b03a36c5452f3cca4
Tavish Armstrong <tavisharmstrong@gmail.com>
2013-11-07 20:48:16 -0500

    Add tests for coaching code

--- tests/test_gitcoach.py
+++ tests/test_gitcoach.py
@@ -9,7 +9,7 @@ Tests for `gitcoach` module.
 """
 
 from nose.tools import eq_
-from gitcoach import learn
+from gitcoach import learn, coach
 
 
 def test_find_correlations():
@@ -50,4 +50,42 @@ def test_find_counts():
         'f6': 2,
     }
     result = learn.find_counts(input)
+    eq_(result, expected_result)
+
+
+def test_find_relevant_correlations():
+    cors = {('f1', 'f2'): 1, ('f2', 'f3'): 2}
+    expected_result = {
+        'f1': 0.25, 'f3': 0.5,
+    }
+    result = coach.find_relevant_correlations(cors, 'f2', 4)
+    eq_(result, expected_result)
+
+
+def test_filter_threshold():
+    cors = {'f1': 0.2, 'f2': 0.3, 'f3': 0.7, 'f4': 0.95}
+    expected_result = {
+        'f4': 0.95,
+    }
+    result = coach.filter_threshold(cors, 0.9)
+    eq_(result, expected_result)
+
+
+def test_filter_threshold_equal():
+    '''Ensure filter_threshold doesn't reject correlations when they are
+    on the threshold.'''
+    cors = {'f1': 0.2, 'f2': 0.3, 'f3': 0.7, 'f4': 0.95}
+    expected_result = {
+        'f4': 0.95,
+    }
+    result = coach.filter_threshold(cors, 0.95)
+    eq_(result, expected_result)
+
+
+def test_filter_threshold_lots():
+    cors = {'f1': 0.2, 'f2': 0.3, 'f3': 0.7, 'f4': 0.95}
+    expected_result = {
+        'f2': 0.3, 'f3': 0.7, 'f4': 0.95
+    }
+    result = coach.filter_threshold(cors, 0.21)
     eq_(result, expected_result)
